eb63ed966a3609c15222884d8938b660237d4f7b
Partha11 <abirpartha7@gmail.com>
2018-07-31 00:19:40 +0600

    updated gui

--- app/src/main/java/com/example/activitytest/MainActivity.java
+++ app/src/main/java/com/example/activitytest/MainActivity.java
@@ -17,13 +17,21 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText fields[];
+
     CheckBox checkBox;
+
     Button submitButton;
+    Button resetButton;
+    Button loginButton;
+
     RadioGroup genderGroup;
     RadioGroup salaryGroup;
+
+    RadioButton genderButton;
+    RadioButton salaryButton;
 
     List<String> skills;
 
@@ -44,45 +52,62 @@ public class MainActivity extends AppCompatActivity {
 
         skills = new ArrayList<>();
 
-        genderGroup = findViewById(R.id.genderGroup);
-        salaryGroup = findViewById(R.id.salaryGroup);
+        initAll();
+
+        submitButton.setOnClickListener(this);
+        resetButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
 
         genderGroup.check(R.id.male);
-
-        initFields();
-
-        submitButton = findViewById(R.id.submitButton);
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick( View view ) {
-
-                //emp = new EmployeeInfo("abc", "abc", "***", "21", "java", "male", "500");
-
-                name = fields[0].getText().toString();
-                mail = fields[1].getText().toString();
-                pass = fields[2].getText().toString();
-                _age = fields[3].getText().toString();
-
-                emp = new EmployeeInfo(name, mail, pass, _age, "java", "male", "500");
-
-                TempData.empInfo.add(emp);
-
-                Intent intent = new Intent(MainActivity.this, Employee.class);
-                startActivity(intent);
-            }
-        });
+        salaryGroup.check(R.id.basedSalary);
     }
 
-    public void initFields() {
+    public void initAll() {
 
         fields = new EditText[4];
 
-        fields[0] = findViewById(R.id.nameField);
-        fields[1] = findViewById(R.id.mailField);
-        fields[2] = findViewById(R.id.passField);
-        fields[3] = findViewById(R.id.ageField);
+        fields[0] = findViewById(R.id.nameField);   //Name
+        fields[1] = findViewById(R.id.mailField);   //Email
+        fields[2] = findViewById(R.id.passField);   //Password
+        fields[3] = findViewById(R.id.ageField);    //Age
+
+        genderGroup = findViewById(R.id.genderGroup);
+        salaryGroup = findViewById(R.id.salaryGroup);
+
+        submitButton = findViewById(R.id.submitButton);
+        resetButton = findViewById(R.id.resetButton);
+        loginButton = findViewById(R.id.loginButton);
+    }
+
+    public void clearAll() {
+
+        skills.clear();
+
+        for (int i = 0; i < 4; i++)
+
+            fields[i].setText("");
+
+        genderGroup.check(R.id.male);
+        salaryGroup.check(R.id.basedSalary);
+    }
+
+    public void fieldToText() {
+
+        name = fields[0].getText().toString();
+        mail = fields[1].getText().toString();
+        pass = fields[2].getText().toString();
+        _age = fields[3].getText().toString();
+
+        skillConcatenated = TextUtils.join(", ", skills);
+
+        int genderIndex = genderGroup.getCheckedRadioButtonId();
+        int salaryIndex = salaryGroup.getCheckedRadioButtonId();
+
+        genderButton = findViewById(genderIndex);
+        salaryButton = findViewById(salaryIndex);
+
+        gender = genderButton.getText().toString();
+        salary = salaryButton.getText().toString();
     }
 
     public void checkboxClicked(View view) {
@@ -94,15 +119,115 @@ public class MainActivity extends AppCompatActivity {
         if (checkBox.isChecked()) {
 
             skills.add(text);
-
-            Toast.makeText(this, text + " added", Toast.LENGTH_SHORT).show();
         }
 
         else {
 
             skills.remove(text);
-
-            Toast.makeText(this, text + " removed", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.submitButton:
+
+                submitButtonClicked(view);
+                break;
+
+            case R.id.resetButton:
+
+                resetButtonClicked(view);
+                break;
+
+            case R.id.loginButton:
+
+                loginButtonClicked(view);
+                break;
+
+            default:
+
+                Toast.makeText(this, "Could Not Click Button", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public int checkFields() {
+
+        if (fields[0].getText().toString().equals("") || fields[0].getText().toString().equals(" "))
+
+            return 1;
+
+        else if (fields[1].getText().toString().equals("") || fields[1].getText().toString().equals(" "))
+
+            return 2;
+
+        else if (fields[2].getText().toString().equals("") || fields[2].getText().toString().equals(" "))
+
+            return 3;
+
+        else if (fields[3].getText().toString().equals("") || fields[3].getText().toString().equals(" "))
+
+            return 4;
+
+        else
+
+            return 0;
+    }
+
+    public void submitButtonClicked(View view) {
+
+        //emp = new EmployeeInfo("abc", "abc", "***", "21", "java", "male", "500");
+
+        fieldToText();
+
+        int validateFields = checkFields();
+
+        if (validateFields == 0) {
+
+            emp = new EmployeeInfo(name, mail, pass, _age, skillConcatenated, gender, salary);
+
+            TempData.getEmpInfo().add(emp);
+
+            Intent intent = new Intent(MainActivity.this, Employee.class);
+            startActivity(intent);
+        }
+
+        else if (validateFields == 1)
+
+            fields[0].setError("Name field can not be empty!!");
+
+        else if (validateFields == 2)
+
+            fields[0].setError("Email field can not be empty!!");
+
+        else if (validateFields == 3)
+
+            fields[0].setError("Password can not be blank!!");
+
+        else if (validateFields == 4)
+
+            fields[0].setError("Age field can not be empty!!");
+    }
+
+    public void resetButtonClicked(View view) {
+
+        clearAll();
+
+        CheckBox javaBox = findViewById(R.id.javaCheck);
+        CheckBox cppBox = findViewById(R.id.cppCheck);
+        CheckBox pythonBox = findViewById(R.id.pythonCheck);
+        CheckBox phpBox = findViewById(R.id.phpCheck);
+
+        javaBox.setChecked(false);
+        cppBox.setChecked(false);
+        pythonBox.setChecked(false);
+        phpBox.setChecked(false);
+    }
+
+    public void loginButtonClicked(View view) {
+
+        Toast.makeText(MainActivity.this, "Log In Button Clicked", Toast.LENGTH_SHORT).show();
     }
 }
